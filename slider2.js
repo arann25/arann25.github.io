26296aeb06cb60c7c765109a8447e893c20b19dd
Justinswk <shuwa106@gmail.com>
2017-09-11 10:25:27 +0800

    11/09

--- slider2.js
+++ slider2.js
@@ -4,11 +4,99 @@ $(document).ready(function(ev){
     if(items>5){
         leftRight=(items-5)*50*-1;
     }
-    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+    $('#custom_carousel1').on('slide.bs.carousel', function (evt) {
         
  
-      $('#custom_carousel .controls li.active').removeClass('active');
-      $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+      $('#custom_carousel1 .controls li.active').removeClass('active');
+      $('#custom_carousel1 .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+    $('.nav').draggable({ 
+        axis: "x",
+         stop: function() {
+            var ml = parseInt($(this).css('left'));
+            if(ml>0)
+            $(this).animate({left:"0px"});
+                if(ml<leftRight)
+                    $(this).animate({left:leftRight+"px"});
+                    
+        }
+      
+    });
+});
+
+
+$(document).ready(function(ev){
+    var items = $(".nav li").length;
+    var leftRight=0;
+    if(items>5){
+        leftRight=(items-5)*50*-1;
+    }
+    $('#custom_carousel2').on('slide.bs.carousel', function (evt) {
+        
+ 
+      $('#custom_carousel2 .controls li.active').removeClass('active');
+      $('#custom_carousel2 .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+    $('.nav').draggable({ 
+        axis: "x",
+         stop: function() {
+            var ml = parseInt($(this).css('left'));
+            if(ml>0)
+            $(this).animate({left:"0px"});
+                if(ml<leftRight)
+                    $(this).animate({left:leftRight+"px"});
+                    
+        }
+      
+    });
+});
+
+
+
+
+
+$(document).ready(function(ev){
+    var items = $(".nav li").length;
+    var leftRight=0;
+    if(items>5){
+        leftRight=(items-5)*50*-1;
+    }
+    $('#custom_carousel3').on('slide.bs.carousel', function (evt) {
+        
+ 
+      $('#custom_carousel3 .controls li.active').removeClass('active');
+      $('#custom_carousel3 .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+    $('.nav').draggable({ 
+        axis: "x",
+         stop: function() {
+            var ml = parseInt($(this).css('left'));
+            if(ml>0)
+            $(this).animate({left:"0px"});
+                if(ml<leftRight)
+                    $(this).animate({left:leftRight+"px"});
+                    
+        }
+      
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(ev){
+    var items = $(".nav li").length;
+    var leftRight=0;
+    if(items>5){
+        leftRight=(items-5)*50*-1;
+    }
+    $('#custom_carousel0').on('slide.bs.carousel', function (evt) {
+        
+ 
+      $('#custom_carousel0 .controls li.active').removeClass('active');
+      $('#custom_carousel0 .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
     })
     $('.nav').draggable({ 
         axis: "x",
